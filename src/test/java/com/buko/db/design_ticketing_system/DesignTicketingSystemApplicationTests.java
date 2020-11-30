7aa2a610e32a43da6466ad4b0d837a7f0e1e6dde
Pudding <1727346009@qq.com>
2020-11-30 22:08:31 +0800

    Initial commit

---
+++ src/test/java/com/buko/db/design_ticketing_system/DesignTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buko.db.design_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignTicketingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
